6ad6ff2e6bf016d270ec94c132bc804396c8d490
Ibrar Ahmed <50750853+Ibrar-Ahmed38@users.noreply.github.com>
2022-01-06 20:29:25 +0500

    added a little counter application in class components

--- src/FunProps.js
+++ src/FunProps.js
@@ -3,9 +3,11 @@ class App extends React.Component {
   constructor()  {
     super();
     this.handleEvent =this.handleEvent.bind(this)
+    this.MinusEvent = this.MinusEvent.bind(this)
     console.log(this, "Constructor")
     this.state = {
       count: 0,
+      deCount : 10,
       ImageUrl: "https://www.gstatic.com/webp/gallery3/1.png",
       tags: ["tag1", "tag2", "tag3"],
     };
@@ -29,7 +31,13 @@ class App extends React.Component {
   }
 
   handleEvent(){
+    this.setState({count : this.state.count + 1})
     console.log("Function Called", this)
+  }
+
+  MinusEvent(){
+    this.setState({deCount : this.state.deCount -1})
+    console.log("Decrement Function called" , this)
   }
   render() {
     return (
@@ -37,8 +45,12 @@ class App extends React.Component {
         <div className="bg-warning">
           {/* <FunProps name={"Halathi"} email={"Lundu"}/> */}
           <img src={this.state.ImageUrl} alt="" />
+         
+          <h1>{this.state.count}</h1>
+          <h1>{this.state.deCount}</h1>
           {this.renderTags()}
-          <button onClick={this.handleEvent}>onClick</button>
+          <button onClick={this.handleEvent}>Increase Number</button>
+          <button onClick={this.MinusEvent}>Decrease Number</button>
         </div>
       </>
     );
