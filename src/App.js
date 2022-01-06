889ae02dd1818f9d532305778c65c2e039be7676
Ibrar Ahmed <50750853+Ibrar-Ahmed38@users.noreply.github.com>
2022-01-06 19:12:23 +0500

    worked in state props conditions

--- src/App.js
+++ src/App.js
@@ -1,57 +1,12 @@
-import React from 'react'
-import PropsExample from './PropsExample'
-import { FunProps } from './FunProps'
-
-const App = () => {
-  return (
-    <div>
-      {/* <PropsExample name={"ibrar"}/> */}
-      <FunProps name={"ibrar"} email={"ibrarahmedjani@gmail.com"} phone={"03423811975"}/>
-    </div>
-  )
+import React from "react";
+import FunProps from './FunProps'
+class App extends React.Component{
+  render(){
+    return(
+      <>
+      <FunProps/>
+      </>
+    )
+  }
 }
-
 export default App
-// import React from 'react'
-// import PropsExample from './PropsExample'
-
-// const todos = [
-//   {
-//     title: 'clean',
-//     done: false,
-//   },
-//   {
-//     title: 'do the dishes',
-//     done: true,
-//   }
-// ];
-
-// class App extends React.Component{
-//   constructor(props){
-//     super(props)
-//     this.state = {
-//       show : true
-//     }
-//   }
-
-//   toggleShow() {
-//     this.setState({
-//       show: !this.state.show
-//     })
-//   }
-
-//   render(){
-// return(<>
-//     <div>
-//       <PropsExample  name={"text"} heading={"Halathi lundu"}/>
-//     {this.state.show &&
-//       <div>show this..</div>
-//       }
-//       <button onClick={() => this.toggleShow()} >Toggle</button>
-  
-//     </div>
-//     </>)
-//   }
-// }
-
-// export default App
--- src/FunProps.js
+++ src/FunProps.js
@@ -1,16 +1,33 @@
-import React, { useState } from 'react'
-
-export const FunProps = (props) => {
-  const [data, setData] = useState("Ibrar")
-  const {name, email, phone} = this.props
-    
-  const setName = ()=>{
-    setData("Ahmed")
+import React  from "react";
+import FunProps from './FunProps'
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      count: 0,
+      ImageUrl : 'https://www.gstatic.com/webp/gallery3/1.png',
+      tags: ['tag1', 'tag2', 'tag3']
+    }
   }
-  return (
-        <div>
-            {data}
-          <h1>{name} {email} {phone}</h1>
-        </div>
+
+  renderTags(){
+    if(this.state.tags.length === 0) return <p>There is No Tags</p>
+    
+    return <ul> {this.state.tags.map(tag => <li>{tag}</li>)} </ul>
+
+  }
+  render(){
+    return(
+      <>
+      <div className="bg-warning">
+      {/* <FunProps name={"Halathi"} email={"Lundu"}/> */}
+      <img src={this.state.ImageUrl} alt="" />
+     {this.renderTags()}
+      </div>
+     
+      </>
     )
+  }
 }
+
+export default App
