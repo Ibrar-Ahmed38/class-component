ade48a4f1b42b5440c19454033b7ade37e0c884b
Ibrar Ahmed <50750853+Ibrar-Ahmed38@users.noreply.github.com>
2022-01-06 20:21:04 +0500

    Worked in a bind

--- src/FunProps.js
+++ src/FunProps.js
@@ -1,33 +1,48 @@
-import React  from "react";
-import FunProps from './FunProps'
-class App extends React.Component{
-  constructor(){
-    super()
-    this.state={
+import React from "react";
+class App extends React.Component {
+  constructor()  {
+    super();
+    this.handleEvent =this.handleEvent.bind(this)
+    console.log(this, "Constructor")
+    this.state = {
       count: 0,
-      ImageUrl : 'https://www.gstatic.com/webp/gallery3/1.png',
-      tags: ['tag1', 'tag2', 'tag3']
-    }
+      ImageUrl: "https://www.gstatic.com/webp/gallery3/1.png",
+      tags: ["tag1", "tag2", "tag3"],
+    };
   }
 
-  renderTags(){
-    if(this.state.tags.length === 0) return <p>There is No Tags</p>
-    
-    return <ul> {this.state.tags.map(tag => <li>{tag}</li>)} </ul>
+  renderTags() {
+    if (this.state.tags.length === 0)
+      return (
+        <>
+          <p>There is No Tags</p> <h1>Please create a new Tag</h1>
+        </>
+      );
 
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
   }
-  render(){
-    return(
+
+  handleEvent(){
+    console.log("Function Called", this)
+  }
+  render() {
+    return (
       <>
-      <div className="bg-warning">
-      {/* <FunProps name={"Halathi"} email={"Lundu"}/> */}
-      <img src={this.state.ImageUrl} alt="" />
-     {this.renderTags()}
-      </div>
-     
+        <div className="bg-warning">
+          {/* <FunProps name={"Halathi"} email={"Lundu"}/> */}
+          <img src={this.state.ImageUrl} alt="" />
+          {this.renderTags()}
+          <button onClick={this.handleEvent}>onClick</button>
+        </div>
       </>
-    )
+    );
   }
 }
 
-export default App
+export default App;
